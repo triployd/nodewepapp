4005ac1a8e554501e9e463302fa3639e0858931a
Xiangxiao Wang <xiangxiao.wang.ce@gmail.com>
2016-07-15 20:42:01 -0500

    this is the real CRUD functionalities

--- nodewebapp/routes/blobs.js
+++ nodewebapp/routes/blobs.js
@@ -78,3 +78,169 @@ router.route('/')
               }
         })
     });
+
+router.route('/:id')
+  .get(function(req, res) {
+    mongoose.model('Blob').findById(req.id, function (err, blob) {
+      if (err) {
+        console.log('GET Error: There was a problem retrieving: ' + err);
+      } else {
+        console.log('GET Retrieving ID: ' + blob._id);
+        var blobdob = blob.dob.toISOString();
+        blobdob = blobdob.substring(0, blobdob.indexOf('T'))
+        res.format({
+          html: function(){
+              res.render('blobs/show', {
+                "blobdob" : blobdob,
+                "blob" : blob
+              });
+          },
+          json: function(){
+              res.json(blob);
+          }
+        });
+      }
+    });
+  });
+
+//GET the individual blob by Mongo ID
+router.get('/:id/edit', function(req, res) {
+    //search for the blob within Mongo
+    mongoose.model('Blob').findById(req.id, function (err, blob) {
+        if (err) {
+            console.log('GET Error: There was a problem retrieving: ' + err);
+        } else {
+            //Return the blob
+            console.log('GET Retrieving ID: ' + blob._id);
+            //format the date properly for the value to show correctly in our edit form
+          var blobdob = blob.dob.toISOString();
+          blobdob = blobdob.substring(0, blobdob.indexOf('T'))
+            res.format({
+                //HTML response will render the 'edit.jade' template
+                html: function(){
+                       res.render('blobs/edit', {
+                          title: 'Blob' + blob._id,
+                        "blobdob" : blobdob,
+                          "blob" : blob
+                      });
+                 },
+                 //JSON response will return the JSON output
+                json: function(){
+                       res.json(blob);
+                 }
+            });
+        }
+    });
+});
+
+//PUT to update a blob by ID
+router.put('/:id/edit', function(req, res) {
+    // Get our REST or form values. These rely on the "name" attributes
+    var name = req.body.name;
+    var badge = req.body.badge;
+    var dob = req.body.dob;
+    var company = req.body.company;
+    var isloved = req.body.isloved;
+
+   //find the document by ID
+        mongoose.model('Blob').findById(req.id, function (err, blob) {
+            //update it
+            blob.update({
+                name : name,
+                badge : badge,
+                dob : dob,
+                isloved : isloved
+            }, function (err, blobID) {
+              if (err) {
+                  res.send("There was a problem updating the information to the database: " + err);
+              } 
+              else {
+                      //HTML responds by going back to the page or you can be fancy and create a new view that shows a success page.
+                      res.format({
+                          html: function(){
+                               res.redirect("/blobs/" + blob._id);
+                         },
+                         //JSON responds showing the updated values
+                        json: function(){
+                               res.json(blob);
+                         }
+                      });
+               }
+            })
+        });
+});
+
+//PUT to update a blob by ID
+router.put('/:id/edit', function(req, res) {
+    // Get our REST or form values. These rely on the "name" attributes
+    var name = req.body.name;
+    var badge = req.body.badge;
+    var dob = req.body.dob;
+    var company = req.body.company;
+    var isloved = req.body.isloved;
+
+   //find the document by ID
+        mongoose.model('Blob').findById(req.id, function (err, blob) {
+            //update it
+            blob.update({
+                name : name,
+                badge : badge,
+                dob : dob,
+                isloved : isloved
+            }, function (err, blobID) {
+              if (err) {
+                  res.send("There was a problem updating the information to the database: " + err);
+              } 
+              else {
+                      //HTML responds by going back to the page or you can be fancy and create a new view that shows a success page.
+                      res.format({
+                          html: function(){
+                               res.redirect("/blobs/" + blob._id);
+                         },
+                         //JSON responds showing the updated values
+                        json: function(){
+                               res.json(blob);
+                         }
+                      });
+               }
+            })
+        });
+});
+
+/* GET New Blob page. */
+router.get('/new', function(req, res) {
+    res.render('blobs/new', { title: 'Add New Blob' });
+});
+
+// route middleware to validate :id
+router.param('id', function(req, res, next, id) {
+    //console.log('validating ' + id + ' exists');
+    //find the ID in the Database
+    mongoose.model('Blob').findById(id, function (err, blob) {
+        //if it isn't found, we are going to repond with 404
+        if (err) {
+            console.log(id + ' was not found');
+            res.status(404)
+            var err = new Error('Not Found');
+            err.status = 404;
+            res.format({
+                html: function(){
+                    next(err);
+                 },
+                json: function(){
+                       res.json({message : err.status  + ' ' + err});
+                 }
+            });
+        //if it is found we continue on
+        } else {
+            //uncomment this next line if you want to see every JSON document response for every GET/PUT/DELETE call
+            //console.log(blob);
+            // once validation is done save the new item in the req
+            req.id = id;
+            // go to the next thing
+            next(); 
+        } 
+    });
+});
+
+module.exports = router;
