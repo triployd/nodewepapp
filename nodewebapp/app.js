7a47cc07cd410ef408648ac18edccad210c5cfff
Xiangxiao Wang <xiangxiao.wang.ce@gmail.com>
2016-07-15 21:00:56 -0500

    added jade templates and routes

--- nodewebapp/app.js
+++ nodewebapp/app.js
@@ -11,6 +11,8 @@ var users = require('./routes/users');
 // added for mongodb:
 var db = require('./model/db');
 var blob = require('./model/blobs');
+// blobs route:
+var blobs = require('./routes/blobs');
 
 var app = express();
 
@@ -19,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+// add blobs
+app.use('/blobs', blobs);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
